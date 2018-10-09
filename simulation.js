2681c9f25ba4ab71f226722572453cedaaafa834
hofm1704 <hofm1704@student.nhl.nl>
2018-10-09 11:39:49 +0200

    bestanden aangemaakt

---
+++ simulation.js
@@ -0,0 +1,66 @@
+function loadOBJModel(modelPath, modelName, texturePath, textureName, onload) {
+    new THREE.MTLLoader()
+        .setPath(texturePath)
+        .load(textureName, function (materials) {
+            materials.preload();
+
+            new THREE.OBJLoader()
+                .setPath(modelPath)
+                .setMaterials(materials)
+                .load(modelName, function (object) {
+                    onload(object);
+                }, function () { }, function (e) { console.log("Error loading model"); console.log(e); });
+        });
+}
+
+window.onload = function () {
+    var camera, scene, renderer;
+    var cameraControls;
+    var lights = new THREE.Group();
+    
+    function init() {
+        camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1500);
+        cameraControls = new THREE.OrbitControls(camera);
+        camera.position.z = 15;
+        camera.position.y = 5;
+        camera.position.x = 15;
+        cameraControls.update();
+        scene = new THREE.Scene();
+
+        renderer = new THREE.WebGLRenderer({ antialias: true });
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setSize(window.innerWidth, window.innerHeight + 5);
+        document.body.appendChild(renderer.domElement);
+
+        window.addEventListener('resize', onWindowResize, false);
+
+        var geometry = new THREE.PlaneGeometry(30, 30, 32);
+        var material = new THREE.MeshPhongMaterial({ color: 0xffffff, side: THREE.DoubleSide });
+        var plane = new THREE.Mesh(geometry, material);
+        plane.rotation.x = Math.PI / 2.0;
+        plane.position.x = 0;
+        plane.position.z = 0;
+        scene.add(plane);
+
+        light = new THREE.AmbientLight(0x404040, 2);
+        lights.add(light);
+        scene.add(lights);
+    }
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        cameraControls.update();
+        renderer.render(scene, camera);
+        lights.updateMatrix();
+        lights.updateMatrixWorld();
+    }
+
+    init();
+    animate();
+}
